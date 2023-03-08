c4a58c9412539d1b0f6d1b8447dc6fb6da193e7d
olegsmit <oleg.smit@gmail.com>
2023-03-08 19:39:24 +0200

    add whl

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='clean_folder',
-    version='0.0.1',
+    version='0.0.2',
     description='HW 07. Clean folder',
     author='OSmit',
     author_email='oleg.smit@gmail.com',
@@ -13,6 +13,7 @@ setup(
         "Operating System :: OS Independent",
     ],
     packages=find_namespace_packages(),
+    include_package_data=True,
     entry_points={'console_scripts': [
         'clean_folder=clean_folder.clean:start']}
 )
